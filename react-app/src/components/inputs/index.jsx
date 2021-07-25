02ce3ed1126538e6934a410bde63b7f915439664
Torjuman <torjumanrimon7@gmail.com>
2021-07-25 12:58:52 +0600

    Added radio button to input section.

--- react-app/src/components/inputs/index.jsx
+++ react-app/src/components/inputs/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class Input extends React.Component {
-
     state = {
         name: '',
         country: '',
         bio: '',
         birthday: '',
+        gender: '',
     }
     handleChange = event => {
         this.setState({[event.target.name]: event.target.value})
@@ -17,20 +17,28 @@ class Input extends React.Component {
 
         return (
             <div>
-                <input className="form-control my-2" type="text" onChange={this.handleChange} value={name} name="name" placeholder="Enter your name"/>
+                <input className="form-control my-2" type="text" name="name" placeholder="Enter your name" value={name} onChange={this.handleChange} />
 
-                <select className="form-control my-2" onChange=     {this.handleChange} value={country} name="country" >
+                <select className="form-control my-2" name="country" value={country} onChange={this.handleChange} >
                     <option >Select your country</option>
-                    <option value="Bangladesh">Bangladesh</option>
+                    <option value="Bnagladesh">Bnagladesh</option>
                     <option value="India">India</option>
                     <option value="Srilanka">Srilanka</option>
                     <option value="Pakistan">Pakistan</option>
                     <option value="China">China</option>
                 </select>
 
-                <textarea className="form-control my-2" onChange={this.handleChange} name="bio" value={bio} placeholder="Write about yourself."></textarea>
+                <textarea className="form-control my-2" name="bio" placeholder="Write about your self" value={bio} onChange={this.handleChange} ></textarea>
+        
+                <input className="form-control my-2" type="date" name="birthday" value={birthday} onChange={this.handleChange} />
 
-                <input className="form-control my-2" onChange={this.handleChange} value={birthday} type="date" name="birthday"/>
+                <div onChange={this.handleChange}>
+                    <input type="radio" name="gender" value="Male" />Male
+
+                    <input type="radio" name="gender" value="Female" />Female
+
+                    <input type="radio" name="gender" value="Other" />Other
+                </div>
 
                 <button onClick={() => console.log(this.state)}>Show data</button>
             </div>
