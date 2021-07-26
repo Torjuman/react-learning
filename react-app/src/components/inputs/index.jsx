262dd84ce4785915ae502e55eac854b9470fe872
Torjuman <torjumanrimon7@gmail.com>
2021-07-26 12:43:39 +0600

    A checkbox has been implemented with agree terms

--- react-app/src/components/inputs/index.jsx
+++ react-app/src/components/inputs/index.jsx
@@ -7,13 +7,17 @@ class Input extends React.Component {
         bio: '',
         birthday: '',
         gender: '',
+        agree: false,
     }
     handleChange = event => {
         this.setState({[event.target.name]: event.target.value})
     }
+    handleCheckBox = event => {
+        this.setState({agree: event.target.checked})
+    }
 
     render () {
-        const {name, country, bio, birthday} = this.state
+        const {name, country, bio, birthday, agree} = this.state
 
         return (
             <div>
@@ -33,11 +37,15 @@ class Input extends React.Component {
                 <input className="form-control my-2" type="date" name="birthday" value={birthday} onChange={this.handleChange} />
 
                 <div onChange={this.handleChange}>
-                    <input type="radio" name="gender" value="Male" />Male
+                    <input type="radio" name="gender" value="Male" /> Male
 
-                    <input type="radio" name="gender" value="Female" />Female
+                    <input type="radio" name="gender" value="Female" /> Female
 
-                    <input type="radio" name="gender" value="Other" />Other
+                    <input type="radio" name="gender" value="Other" /> Other
+                </div>
+
+                <div>
+                    <input type="checkbox" name="agree" checked={agree} onChange={this.handleCheckBox} /> I agree to all the terms and conditions.
                 </div>
 
                 <button onClick={() => console.log(this.state)}>Show data</button>
